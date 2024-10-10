4ba16a96d05029ff4185b0d5faaac978fde84e09
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2024-10-10 19:11:47 +0200

    style: highlight current cursor reference

--- nvim/init.lua
+++ nvim/init.lua
@@ -786,6 +786,9 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       require('nordic').load()
+      vim.api.nvim_set_hl(0, 'LspReferenceText', { bg = '#4c4846', underline = true })
+      vim.api.nvim_set_hl(0, 'LspReferenceRead', { bg = '#4c4846', underline = true })
+      vim.api.nvim_set_hl(0, 'LspReferenceWrite', { bg = '#4c4846', underline = true })
     end,
   },
 

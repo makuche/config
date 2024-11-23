e979a71c920428eaeb1de50ee4a6cdc49fc3e5c1
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2024-11-23 17:36:33 +0100

    style: apply alejandra nix formatter

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,139 +1,158 @@
-{ config, pkgs, ... }:
+{ config
+, pkgs
+, ...
+}:
 let
-  user = builtins.getEnv "USER";    # Use same setup with different users
-  configDir = if pkgs.stdenv.isDarwin
+  user = builtins.getEnv "USER"; # Use same setup with different users
+  configDir =
+    if pkgs.stdenv.isDarwin
     then "/Users/${user}/.config/dotfiles"
     else "/home/${user}/.config/dotfiles";
   unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
     config = config.nixpkgs.config;
   };
-  python-packages = ps: with ps; [
-    numpy
-    debugpy
-    matplotlib
-    pip
-  ] ++ (if !pkgs.stdenv.isDarwin then [
-    jupyter
-    notebook
-  ] else []);
+  python-packages = ps:
+    with ps;
+    [
+      numpy
+      debugpy
+      matplotlib
+      pip
+    ]
+    ++ (
+      if !pkgs.stdenv.isDarwin
+      then [
+        jupyter
+        notebook
+      ]
+      else [ ]
+    );
   nixgl = import (fetchTarball "https://github.com/guibou/nixGL/archive/main.tar.gz") { inherit pkgs; };
-  nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-    mkdir $out
-    ln -s ${pkg}/* $out
-    rm $out/bin
-    mkdir $out/bin
-    for bin in ${pkg}/bin/*; do
-     wrapped_bin=$out/bin/$(basename $bin)
-     echo "#!${pkgs.bash}/bin/bash" > $wrapped_bin
-     echo "exec ${nixgl.auto.nixGLDefault}/bin/nixGL $bin \"\$@\"" >> $wrapped_bin
-     chmod +x $wrapped_bin
-    done
-  '';
+  nixGLWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "#!${pkgs.bash}/bin/bash" > $wrapped_bin
+       echo "exec ${nixgl.auto.nixGLDefault}/bin/nixGL $bin \"\$@\"" >> $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
 in
 {
-    imports = [
+  imports = [
     ../modules/symlinks.nix
   ];
   # This is needed as long as unstable.neovim is used
   home.enableNixpkgsReleaseCheck = false;
 
   home.username = user;
-  home.homeDirectory = if pkgs.stdenv.isDarwin
+  home.homeDirectory =
+    if pkgs.stdenv.isDarwin
     then "/Users/${user}"
     else "/home/${user}";
   home.stateVersion = "24.05";
 
-  home.packages = with pkgs; [
-    # Core utilities
-    bat
-    diff-so-fancy
-    direnv
-    eza
-    fd
-    fzf
-    htop
-    jq
-    lima
-    mcfly
-    pv
-    ranger
-    ripgrep
-    tree
-    wget
-    zathura
-    xclip
-    zsh
-    zoxide
+  home.packages = with pkgs;
+    [
+      # Core utilities
+      bat
+      diff-so-fancy
+      direnv
+      eza
+      fd
+      fzf
+      htop
+      jq
+      lima
+      mcfly
+      pv
+      ranger
+      ripgrep
+      tree
+      wget
+      zathura
+      xclip
+      zsh
+      zoxide
 
-    # Development
-    cargo
-    git
-    devbox
+      # Development
+      alejandra
+      cargo
+      git
+      devbox
 
-    # docker-client
-    gh
-    go
-    jdk
-    lazygit
-    lua
-    luarocks
-    minikube
-    nixfmt-rfc-style
-    nodejs_22
-    rustc
-    sqlite
-    tokei
+      # docker-client
+      gh
+      go
+      jdk
+      lazygit
+      lua
+      luarocks
+      minikube
+      nixd
+      nodejs_22
+      rustc
+      sqlite
+      tokei
 
-    # Python development
-    virtualenv
-    (python312.withPackages python-packages)
+      # Python development
+      virtualenv
+      (python312.withPackages python-packages)
 
-    # Terminal enhancements
-    figlet
-    neofetch
-    starship
-    tmux
+      # Terminal enhancements
+      figlet
+      neofetch
+      starship
+      tmux
 
-    # File management
-    fdupes
+      # File management
+      fdupes
 
-    # Text processing
-    gnupg
+      # Text processing
+      gnupg
 
-    # Multimedia
-    ffmpeg-full
-    spotify
-    spotify-player
-    yt-dlp
+      # Multimedia
+      ffmpeg-full
+      spotify
+      spotify-player
+      yt-dlp
 
-    # GUI applications
-    anki-bin
-    obsidian
-    sioyek
-    sqlitebrowser
-    vscode  # needed for working on microcontroller
+      # GUI applications
+      anki-bin
+      obsidian
+      sioyek
+      sqlitebrowser
+      vscode # needed for working on microcontroller
 
-    # Presentation and documentation
-    marp-cli
+      # Presentation and documentation
+      marp-cli
 
-    # Network tools
-    arp-scan
-    nmap
-    mosquitto
-    openvpn
+      # Network tools
+      arp-scan
+      nmap
+      mosquitto
+      openvpn
 
-    # Commented out (to be addressed)
-    # zsh-syntax-highlighting   # FIXME: This had to be installed via brew
-  ] # macOS specific applications
-    ++ (if pkgs.stdenv.isDarwin then [
-      alacritty
-      hidden-bar
-      maccy
-      unstable.neovim
-    ] else [
+      # Commented out (to be addressed)
+      # zsh-syntax-highlighting   # FIXME: This had to be installed via brew
+    ] # macOS specific applications
+    ++ (
+      if pkgs.stdenv.isDarwin
+      then [
+        alacritty
+        hidden-bar
+        maccy
+        unstable.neovim
+      ]
+      else [
         (nixGLWrap pkgs.alacritty)
         neovim
-      ]);
+      ]
+    );
 
   home.file = {
     ".zshrc" = {
@@ -145,10 +164,10 @@ in
 
     ".gitconfig" = {
       source = "${configDir}/gitconfig";
-    };  # TODO: Add alacritty config here as well
+    }; # TODO: Add alacritty config here as well
   };
   home.sessionPath = [ "$HOME/.local/bin" ];
   programs.home-manager.enable = true;
 
-  nixpkgs.config.allowUnfree = true;    # Allow unfree packages (obsidian)
+  nixpkgs.config.allowUnfree = true; # Allow unfree packages (obsidian)
 }

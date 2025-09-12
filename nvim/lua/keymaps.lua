96e3483532a84a34aea133f7fccb4fbcd2f99f3a
Manuel Kuchelmeister <makuche-github@pm.me>
2025-09-12 17:41:33 +0200

    feat(nvim): add helper shortcuts as keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,6 +6,16 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>f', function()
   require('conform').format { bufnr = 0 }
 end)
+
+-- save stuff without formatting, useful to avoid large file changes in repos
+-- that use e.g. different convention
+vim.keymap.set(
+  'n',
+  '<leader>wq',
+  '<cmd>set eventignore=BufWritePre | wq<CR>',
+  { desc = 'Save and quit without auto formatting the current buffer (BufWritePre events)' }
+)
+
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
@@ -14,13 +24,14 @@ vim.keymap.set('n', '<leader>zig', '<cmd>LspRestart<cr>')
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- Open filetree
+-- open filetree
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- this requires tmux-sessionizer located under /usr/local/bin/tmux-sessionizer
 -- TODO: Same is used within tmux, check whether to use it only within tmux
 vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww $HOME/.local/bin/tmux-sessionizer<CR>')
 
+-- move blocks up/down (including indentation!!)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
@@ -29,7 +40,10 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 vim.keymap.set('x', '<leader>p', [["_dP]])
 
-vim.keymap.set('n', '<leader>ss', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- substitute all occurrences of word under cursor
+vim.keymap.set('n', '<leader>ss', [[:%s/\<<c-r><c-w>\>/<c-r><c-w>/gi<left><left><left>]])
+
+-- sources (so) the current buffer
 vim.keymap.set('n', '<leader><leader>', function()
   vim.cmd 'so'
 end)

dad93de8fb6d87ab9976ea15ed6718bef5689664
Manuel Kuchelmeister <makuche-github@pm.me>
2026-02-06 16:23:07 +0100

    build: update flake

--- flake.nix
+++ flake.nix
@@ -7,15 +7,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    # user environments via home manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # macOS config via nix-darwin
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
